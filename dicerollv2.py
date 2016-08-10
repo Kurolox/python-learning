63dece8ad48f3a1c1be0e3c03382d953ad1c1f01
Kurolox <kurolox@gmail.com>
2016-08-10 23:51:11 +0200

    Now modifier is optional. If you introduce a wrongly-formatted input it will ask you to input again rather than just crash.

--- dicerollv2.py
+++ dicerollv2.py
@@ -4,26 +4,37 @@ import re
 
 def get_input():
     print("Insert the number of dices you want to roll (format xdY+Z.)")
-    diceinput = input()
-    pattern = re.compile(r"(\d+)(d|D)(\d+)(\+|-)(\d+)")
-    mo = pattern.search(diceinput)
-    dicelist = [mo.group(1), mo.group(3), mo.group(4), mo.group(5)]
-    return dicelist
+    while True:
+        diceinput = input()
+        pattern = re.compile(r"(\d+)(d|D)(\d+)((\+|-)(\d+))?")
+        mo = pattern.search(diceinput)
+        if mo is not None:
+            dicelist = {"nodices": int(mo.group(1)),
+                        "dnumber": int(mo.group(3))}
+            if mo.group(4) is None:
+                dicelist["modifier"] = 0
+            else:
+                dicelist["modifier"] = int(mo.group(4))
+            return dicelist
+        else:
+            print("That's not a correct input. Try again.")
+    #    else:
+    #        print("That's not a valid input. Try again.")
 
 
-def rolling_dices(diceformat):
+def rolling_dices(dicedict):
     actualdiceroll = 0
     totaldiceroll = 0
-    print("Rolling " + str(diceformat[0]) +
-          " " + str(diceformat[1]) + "-sided dices with a"
-          " modifier of " + str(diceformat[2]) + str(diceformat[3]) + ".\n")
-    for dice in range(int(diceformat[0])):
-        actualdiceroll = random.randint(1, int(diceformat[1]))
+    print("Rolling " + str(dicedict["nodices"]) +
+          " " + str(dicedict["dnumber"]) + "-sided dices with a"
+          " modifier of " + str(dicedict["modifier"]) + ".\n")
+    for dice in range(dicedict["nodices"]):
+        actualdiceroll = random.randint(1, dicedict["dnumber"])
         totaldiceroll += actualdiceroll
         print(actualdiceroll, end=" ")
     print("\n\nTotal without modifier: " + str(totaldiceroll))
     print("Total with modifier: " +
-          str(int(totaldiceroll + int(str(diceformat[2] + str(diceformat[3]))))))
+          str(totaldiceroll + dicedict.get("modifier", 0)))
 
 
 rolling_dices(get_input())
